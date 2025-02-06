e0ed11e4eeccb2413aa7462bda2eb2ba6e68d5f9
Prashant <r56.prashant@gmail.com>
2025-02-06 12:48:41 +0545

    fixes

--- src/app/(payload)/api/page-data/route.ts
+++ src/app/(payload)/api/page-data/route.ts
@@ -6,9 +6,9 @@ export async function GET() {
     const payload = await getPayload({ config })
 
     const [participants, activities, stalls] = await Promise.all([
-        await payload.find({ collection: 'participants', limit: 50 }),
-        await payload.find({ collection: 'activities', limit: 50 }),
-        await payload.find({ collection: 'stalls', limit: 50 }),
+        await payload.find({ collection: 'participants', limit: 5000 }),
+        await payload.find({ collection: 'activities', limit: 10000 }),
+        await payload.find({ collection: 'stalls', limit: 500 }),
     ])
 
     const data = {
